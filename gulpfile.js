e7c258bfdfbe41336e3d7f7b31b479102e4e8059
AlexVonB <AlexVonB@users.noreply.github.com>
2015-05-28 01:28:44 +0200

    doc generation added

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,16 @@
 var gulp = require('gulp');
 var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
+var run = require('gulp-run');
 
 gulp.task('default', function() {
   return gulp.src('jquery.res.js')
     .pipe(uglify())
     .pipe(rename({ extname: '.min.js' }))
-    .pipe(gulp.dest('./'));
+    .pipe(gulp.dest('./'))
+    .pipe(run(
+      'jsdoc -c conf.json'
+    ));
 });
 
 gulp.task('watch', function(){
