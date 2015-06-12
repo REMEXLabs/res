16522aef34238ca04732d31bcb642fb6ce831377
AlexVonB <AlexVonB@users.noreply.github.com>
2015-06-12 22:52:32 +0200

    upload implemented, https only, fail returns object

--- jquery.res.js
+++ jquery.res.js
@@ -14,6 +14,10 @@
 
     // Make sure the url ends with a /
     url = /\/$/.exec(url) ? url : url + '/';
+    // HTTPS only! (Because uploads and security in general)
+    if (!/^https/i.test(url)) {
+      throw 'HTTPS only! The url ' + url + ' is not secure!';
+    }
 
     // Defaults to be used, if no options are given
     var defaults = {
@@ -65,14 +69,13 @@
       for (var i = 0; i < this.queries.length; i++) {
         $queries.append(this.queries[i].toXML());
       }
-      var xmlString = (new XMLSerializer()).serializeToString($queries[0]);
-      // Workaround, the server allows only exactly '<queries>'...
-      return xmlString.replace(' xmlns="http://www.w3.org/1999/xhtml"', '');
+      return _serializeXML($queries);
     };
 
     /**
      * Send the queries to the resource server
-     * @returns {jQuery.Deferred} A Promise (see {@link https://api.jquery.com/category/deferred-object/})
+     * @returns {jQuery.Deferred} A Promise
+     * (see {@link https://api.jquery.com/category/deferred-object/})
      */
     Queries.prototype.send = function() {
       var $deferred = $.Deferred();
@@ -99,7 +102,10 @@
       }).done(function(data){
         $deferred.resolve(new Responses($(data)));
       }).fail(function(xhr, status, error){
-        $deferred.reject(xhr, status, error);
+        $deferred.reject({
+          error: error,
+          status: xhr.status
+        });
       });
 
       return $deferred.promise();
@@ -410,8 +416,8 @@
     /**
      * Shortcut to get the first global URI of the first resource of the first
      * response
-     * @return {string|null} The very first global URI in these responses. Null if
-     * there are no global URIs available (e.g. reference expired)
+     * @return {string|null} The very first global URI in these responses. Null
+     * if there are no global URIs available (e.g. reference expired)
      */
     Responses.prototype.getVeryFirstGlobalAt = function() {
       return this.firstResponse().firstResource().getFirstGlobalAt();
@@ -539,9 +545,247 @@
       return this.globalAts.length > 0 ? this.globalAts[0] : null;
     };
 
+    /**
+     * Upload factory, able to generate child {@link $.res~UploadResource}s
+     * @property {list} resources List of all {@link $.res~UploadResource}s
+     * @constructor
+     */
+    function Upload() {
+      this.resources = [];
+      this._dataReadyCounter = 0;
+      this._tmpXML = null;
+      this._$deferred = null;
+    }
+
+    /**
+     * Adds a {@link $.res~UploadResource} to this upload
+     * @returns {$.res~UploadResource} The {@link $.res~UploadResource}
+     * that can be modified. Use {@link $.res~UploadResource#complete} to
+     * return the parent {@link $.res~Upload}
+     */
+    Upload.prototype.addResource = function() {
+      var r = new UploadResource(this);
+      this.resources.push(r);
+      return r;
+    };
+
+    /**
+     * This gets called if an asynchronous FileReader.readAsDataUrl-call is done
+     * @private
+     */
+    Upload.prototype._dataReady = function() {
+      if (++this._dataReadyCounter === this.resources.length) {
+        this._realSend();
+      }
+    };
+
+    /**
+     * Send the upload resources to the resource server
+     * @returns {jQuery.Deferred} A Promise
+     * (see {@link https://api.jquery.com/category/deferred-object/})
+     */
+    Upload.prototype.send = function() {
+      this._tmpXML = $('<upload></upload>');
+      for (var i = 0; i < this.resources.length; i++) {
+        this._tmpXML.append(this.resources[i].toXML());
+      }
+      this._$deferred = $.Deferred();
+      return this._$deferred.promise();
+    };
+
+    /**
+     * Gets called, if all FileReaders are done, sends the data to the server
+     * for real
+     * @private
+     */
+    Upload.prototype._realSend = function() {
+      var data = _serializeXML(this._tmpXML);
+      var that = this;
+      this._tmpXML = null; // Avoid memory hogging
+      this._dataReadyCounter = 0;
+
+      $.ajax({
+        url: url + 'upload',
+        contentType: 'text/xml', // datatype of data sent
+        crossDomain: true, // tell jQuery CORS is ok
+        data: data, // send plain xml data
+        dataType: 'xml', // expected datatype of the response
+        method: 'POST',
+        password: settings.password,
+        processData: false, // dont convert the xml data sent
+        username: settings.username
+      }).done(function(data){
+        var responses = [];
+        $(data).find('resource').each(function() {
+          responses.push({
+            status: $(this).attr('status'),
+            name: $(this).attr('name'),
+            message: $(this).find('message').text()
+          });
+        });
+        that._$deferred.resolve(responses);
+      }).fail(function(xhr, status, error){
+        that._$deferred.reject({
+          error: error,
+          status: xhr.status
+        });
+      });
+    };
+
+    /**
+    * Describes a resource that can be sent to a resserver
+    *
+    * @param {$.res~Upload} parent Parent {@link $.res~Upload}
+    * @property {String} name The name of the resource
+    * @constructor
+    */
+    function UploadResource(parent) {
+      this._parent = parent;
+      this.name = null;
+      this.file = null;
+      this.inherit = undefined;
+      this.props = [];
+    }
+
+    /**
+     * Return to the parent {@link $.res~Upload} object, i.e. in order to add
+     * another {@link $.res~UploadResource}.
+     * @returns {$.res~Upload} The parent {@link $.res~Upload}
+     */
+    UploadResource.prototype.complete = function() {
+      return this._parent;
+    };
+
+    /**
+     * Set the resources name (which is referenced in the response)
+     * @param {String} name Name of the resource
+     * @return {$.res~UploadResource} The active {@link $.res~UploadResource}
+     */
+    UploadResource.prototype.setName = function(name) {
+      this.name = name;
+      return this;
+    };
+
+    /**
+     * Set if the properties should be inherited from the current version of
+     * the resource
+     * @param {Boolean} name Whether or not the properties should be inherited
+     * @return {$.res~UploadResource} The active {@link $.res~UploadResource}
+     */
+    UploadResource.prototype.setInherit = function(inherit) {
+      this.inherit = inherit;
+      return this;
+    };
+
+    /**
+     * Set the jQuery-object for the html file input element
+     * (like `$('#fileInput')`)
+     * @param {jQuery-object} $fileInnput The file input element
+     * @return {$.res~UploadResource} The active {@link $.res~UploadResource}
+     */
+    UploadResource.prototype.setFileInput = function($fileInput) {
+      if (!window.File || !window.FileReader ||
+        !window.FileList || !window.Blob) {
+        console.log('The File APIs are not fully supported in this browser.');
+        return this;
+      }
+      if ($fileInput.length === 0) {
+        console.log('Could not find file input.');
+        return this;
+      }
+      if (!$fileInput[0].files[0]) {
+        console.log('No file selected');
+        return this;
+      }
+      this.file = $fileInput[0].files[0];
+      return this;
+    };
+
+    /**
+     * Add a query property describing the uploaded resource
+     * @param {string} name  Property name as full URL or without namespace
+     * @param {string} value Desired value for the property
+     * @param {list}   descs Descriptors describing this property (optional)
+     * ```javascript
+     * [
+     *   { name: "desc1", value: "dvalue1" },
+     *   { name: "desc2", value: "dvalue2" }
+     * ]
+     * ```
+     * @return {$.res~UploadResource} The active {@link $.res~UploadResource}
+     */
+    UploadResource.prototype.addProp = function(name, value, descs) {
+      this.props.push(new Prop(this, name, value, descs));
+      return this;
+    };
+
+    /**
+     * Add a query property describing the uploaded resource and switch to the
+     * property in order to add descriptors.
+     * @param {string} name  Property name as full URL or without namespace
+     * @param {string} value Desired value for the property
+     * @return {$.res~Prop} The active {@link $.res~Prop} in order to add
+     * descriptors. Return with {@link $.res~Prop#complete} to the parent
+     * {@link $.res~UploadResource}.
+     */
+    UploadResource.prototype.addPropWithDescs = function(name, value) {
+      var prop = new Prop(this, name, value);
+      this.props.push(prop);
+      return prop;
+    };
+
+    /**
+     * Creates a XML-Document representing this upload resource
+     * @returns {XMLDoc} The XML-Document
+     */
+    UploadResource.prototype.toXML = function() {
+      var that = this;
+      var $resource = $('<resource></resource>');
+
+      var $props = $('<props></props>')
+        .attr('inherit', this.inherit);
+
+      for (var i = 0; i < this.props.length; i++) {
+        $props.append(this.props[i].toXML());
+      }
+
+      $resource.append($props);
+
+      var resData = $('<resourceData></resourceData>')
+        .append($('<name></name>').text(this.name));
+
+      var reader  = new FileReader();
+      reader.onloadend = function () {
+        var dataRegex = /^data:([^\/,]+\/[^,;]+)?(;[^,]+)?,(.*)$/g;
+        var dataArray = dataRegex.exec(reader.result);
+        resData.append($('<data></data>').text(dataArray[3]));
+        that._parent._dataReady();
+      };
+      reader.readAsDataURL(this.file);
+
+      $resource.append(resData);
+
+      return $resource;
+    };
+
+    /**
+     * Serialize an jQuery-XMLDocument
+     * @param  {XMLDoc} $xml The jQuery-XMLDocument
+     * @return {String}      The String representation of the given
+     * jQuery-XMLDocument
+     */
+    function _serializeXML($xml) {
+      var xmlString = (new XMLSerializer()).serializeToString($xml[0]);
+      // Workaround, the server allows only exactly '<queries>'
+      return xmlString.replace(/^<(queries|upload)([^>]*)>/g, '<$1>');
+    }
+
     return {
       queries: function () {
         return new Queries();
+      },
+      upload: function() {
+        return new Upload();
       }
     };
   };
